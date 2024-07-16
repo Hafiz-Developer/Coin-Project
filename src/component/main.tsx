8d238b8b33b28ef87f1290dd0f26695142a08db8
Hafiz Ahmad <ahmaddeveloper122@gmail.com>
2024-07-16 16:38:35 +0500

    a

--- src/component/main.tsx
+++ src/component/main.tsx
@@ -1,87 +1,95 @@
-import { useEffect, useState } from 'react';
-import Coin from '../assets/images/coin.webp'
-import { FaBitcoin } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import Coin from "../assets/images/coin.png";
+import textImage from "../assets/images/textImage.png";
 interface FloatingText {
-	id: number;
-	x: number;
-	y: number;
+  id: number;
+  x: number;
+  y: number;
 }
 
 const Main = () => {
-    const maxEnergy = 2000;
-	const [value, setValue] = useState(0);
-	const [energy, setEnergy] = useState(maxEnergy);
-	const [floatingTexts, setFloatingTexts] = useState<FloatingText[]>([]);
-	const [nextId, setNextId] = useState(0);
+  const maxEnergy = 2000;
+  const [value, setValue] = useState(0);
+  const [energy, setEnergy] = useState(maxEnergy);
+  const [floatingTexts, setFloatingTexts] = useState<FloatingText[]>([]);
+  const [nextId, setNextId] = useState(0);
 
-	const handleClick = (e: any) => {
-		if (energy > 0) {
-			setValue((prev) => prev + 1);
-			setEnergy((prev) => (prev > 0 ? prev - 1 : 0));
+  const handleClick = (e: any) => {
+    if (energy > 0) {
+      setValue((prev) => prev + 1);
+      setEnergy((prev) => (prev > 0 ? prev - 1 : 0));
 
-			const { clientX: x, clientY: y } = e;
-			const newText: FloatingText = { id: nextId, x, y };
-			setFloatingTexts((prev) => [...prev, newText]);
-			setNextId((prev) => prev + 1);
+      const { clientX: x, clientY: y } = e;
+      const newText: FloatingText = { id: nextId, x, y };
+      setFloatingTexts((prev) => [...prev, newText]);
+      setNextId((prev) => prev + 1);
 
-			setTimeout(() => {
-				setFloatingTexts((prev) => prev.filter((text) => text.id !== newText.id));
-			}, 2000);
-		}
-	};
+      setTimeout(() => {
+        setFloatingTexts((prev) =>
+          prev.filter((text) => text.id !== newText.id)
+        );
+      }, 2000);
+    }
+  };
 
-	const energyPercentage = (energy / maxEnergy) * 100;
+  const energyPercentage = (energy / maxEnergy) * 100;
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setEnergy((prevEnergy) => {
-				if (prevEnergy < maxEnergy) {
-					return prevEnergy + 1;
-				}
-				return prevEnergy;
-			});
-		}, 1500);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setEnergy((prevEnergy) => {
+        if (prevEnergy < maxEnergy) {
+          return prevEnergy + 1;
+        }
+        return prevEnergy;
+      });
+    }, 1500);
 
-		return () => clearInterval(interval);
-	}, []);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-    <div className="mainData">
+      <div className="mainData">
         <div className="TotalCoins">
-            <FaBitcoin className='iconCoin'/>
-            {value.toLocaleString()}
+          <img src={textImage} alt="" />
+          {value.toLocaleString()}
         </div>
         <div className="mainImgCoin">
-            <img src={Coin}
-            	onClick={handleClick}
-                className='coin-button'
-            alt="" />
+          <img
+            src={Coin}
+            onClick={handleClick}
+            onMouseDown={(e) => e.preventDefault()}
+            draggable={true}
+            className="coin-button .no-select"
+            alt="Puma"
+          />
         </div>
         <div className="energy">
-            <div className="energyText">
-                <h2>energy</h2>
-                <span>{energy} / {maxEnergy}</span>
-            </div>
-            <div className="percentage">
-                <div className="PercenetCount"
-                style={{ width: `${energyPercentage}%` }}
-                >
-
-                </div>
-            </div>
+          <div className="energyText">
+            <h2>energy</h2>
+            <span>
+              {energy} / {maxEnergy}
+            </span>
+          </div>
+          <div className="percentage">
+            <div
+              className="PercenetCount"
+              style={{ width: `${energyPercentage}%` }}
+            ></div>
+          </div>
         </div>
         {floatingTexts.map((text) => (
-				<span
-					className="floating-text font-semibold text-[30px]"
-					key={text.id}
-					style={{ top: text.y, left: text.x }}
-				>
-					+1
-				</span>
-			))}
-    </div>
+          <span
+            className="floating-text font-semibold text-[30px]"
+            key={text.id}
+            style={{ top: text.y, left: text.x }}
+          >
+            +1
+          </span>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
