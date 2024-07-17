b2a61b81c0e7b250efb7029f2240931cc868ab79
Hafiz Ahmad <ahmaddeveloper122@gmail.com>
2024-07-18 00:30:58 +0500

    a

--- src/component/lastTask.tsx
+++ src/component/lastTask.tsx
@@ -53,6 +53,21 @@ const LastTask = () => {
           <IoIosArrowForward />
         </div>
       </div>
+      <div className="lastTask_Click ">
+        <div className="lastTask_first">
+          <img src={ticktok} alt="" />
+          <div className="lastTask_text">
+            <h1>Join Fcaebook Group</h1>
+            <div className="lastTask_insideText">
+              <img src={textImage} alt="" />
+              +100,000
+            </div>
+          </div>
+        </div>
+        <div className="lastTask_second">
+          <IoIosArrowForward />
+        </div>
+      </div>
       
     </div>
   );
